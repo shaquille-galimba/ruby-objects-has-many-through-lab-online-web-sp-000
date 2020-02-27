8fce736219a2cb854c49105ebdcc5fe7bdc19068
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-27 05:51:18 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,13 +1,25 @@
-class Appointment
-
+class Patient
+  attr_accessor :name
   @@all = []
 
-  def initialize(date, patient, doctor)
-    @date, @patient, @doctor = date, patient, doctor
+  def initialize(name)
+    @name = name
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def new_appointment(date, doctor)
+    new_appointment = Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|a| a.patient == self}
+  end
+
+  def doctors
+    appointments.map {|a| a.doctor}
   end
 end
