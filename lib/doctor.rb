6f93307c82d0263e0bfa143c533f7622abe8c0f4
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-27 05:50:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
   end
 
   def new_appointment(date, patient)
-    new_appointment = Appointment.new(date, patient, self)
+    Appointment.new(date, patient, self)
   end
 
   def appointments
